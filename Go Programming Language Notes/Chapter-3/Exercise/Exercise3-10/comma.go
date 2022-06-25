62eeef50037345e885387c0aa473ecd9d1a4c06d
zinedburak <zined_burak@hotmail.com>
2022-06-25 12:58:20 +0300

    Exercise 3.10 final work

--- Go Programming Language Notes/Chapter-3/Exercise/Exercise3-10/comma.go
+++ Go Programming Language Notes/Chapter-3/Exercise/Exercise3-10/comma.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"fmt"
+)
 
 func main() {
-	fmt.Println(comma("123456789"))
+	fmt.Println(comma("12345678"))
+	fmt.Println(commaWByte("12345678"))
 }
 
 func comma(s string) string {
@@ -12,4 +16,27 @@ func comma(s string) string {
 		return s
 	}
 	return comma(s[:n-3]) + "," + s[n-3:]
+}
+
+// Exercise 3.10
+func commaWByte(s string) string {
+	length := len(s)
+	if length <= 3 {
+		return s
+	}
+	var buf bytes.Buffer
+	reminder, times := length%3, length/3
+
+	if reminder != 0 {
+		buf.WriteString(s[:reminder])
+		buf.WriteString(",")
+	}
+
+	for i := 0; i < times; i++ {
+		buf.WriteString(s[i*3+reminder : i*3+reminder+3])
+		if i != times-1 {
+			buf.WriteString(",")
+		}
+	}
+	return buf.String()
 }
