3e091c43c079fa7a95cef539869c08e29decc0fa
zinedburak <zined_burak@hotmail.com>
2022-06-25 08:01:24 +0300

    Exercise 3.3 final work

--- Go Programming Language Notes/Chapter-3/Exercise/surface.go
+++ Go Programming Language Notes/Chapter-3/Exercise/surface.go
@@ -29,12 +29,18 @@ func main() {
 	fmt.Printf(startString)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			ax, ay := corner(i+1, j)
-			bx, by := corner(i, j)
-			cx, cy := corner(i, j+1)
-			dx, dy := corner(i+1, j+1)
-			polygonPoint := fmt.Sprintf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
-				ax, ay, bx, by, cx, cy, dx, dy)
+			ax, ay, aIsPeak := corner(i+1, j)
+			bx, by, bIsPeak := corner(i, j)
+			cx, cy, cIsPeak := corner(i, j+1)
+			dx, dy, dIsPeak := corner(i+1, j+1)
+			var color string
+			if aIsPeak || bIsPeak || cIsPeak || dIsPeak {
+				color = "#f00"
+			} else {
+				color = "#00f"
+			}
+			polygonPoint := fmt.Sprintf("<polygon points='%g,%g %g,%g %g,%g %g,%g' style='stroke: %s'/>\n",
+				ax, ay, bx, by, cx, cy, dx, dy, color)
 			htmlSVG.WriteString(polygonPoint)
 			fmt.Printf(polygonPoint)
 		}
@@ -44,31 +50,38 @@ func main() {
 	htmlSVG.Sync()
 }
 
-func corner(i, j int) (float64, float64) {
+func corner(i, j int) (float64, float64, bool) {
 	// Find point (x,y) at corner of cell (i,j).
 	x := xyRange * (float64(i)/cells - 0.5)
 	y := xyRange * (float64(j)/cells - 0.5)
 	// Compute surface height z.
 
-	// Original Shape
-	//z := f(x, y)
+	//Original Shape
+	z, isPeak := f(x, y)
 
 	// Exercise 3.2
-	z := f2(x, y)
+	//z := f2(x, y)
 
 	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
 	sx := width/2 + (x-y)*cos30*xyScale
 	sy := height/2 + (x+y)*sin30*xyScale - z*zScale
-	return sx, sy
+	return sx, sy, isPeak
 }
-func f(x, y float64) float64 {
+func f(x, y float64) (float64, bool) {
 	r := math.Hypot(x, y) // distance from (0,0)
 
 	// Exercise 3.1
 	if math.IsNaN(math.Sin(r)/r) || math.IsInf(math.Sin(r)/r, 0) {
-		return 0.0
+		return 0.0, isPeak(r)
 	}
-	return math.Sin(r) / r
+	return math.Sin(r) / r, isPeak(r)
+}
+
+func isPeak(r float64) bool {
+	if math.Abs(r-math.Tan(r)) < 3 {
+		return true
+	}
+	return false
 }
 
 // Exercise 3.2
